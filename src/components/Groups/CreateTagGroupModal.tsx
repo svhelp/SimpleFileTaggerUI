25f8a9d913a5e1c88fdcd6b701c7f85f04ba96ce
Aleksandr Kravchenko <svhelpp@gmail.com>
2022-09-29 19:44:41 +0200

    - added groups page;

---
+++ src/components/Groups/CreateTagGroupModal.tsx
@@ -0,0 +1,22 @@
+import { Input, Modal } from "antd";
+import { useState } from "react";
+
+interface ICreateTagGroupModalProps {
+    isModalOpen: boolean;
+    onCreate: (gropName: string) => void;
+    closeModal: () => void;
+}
+
+export const CreateTagGroupModal = (props: ICreateTagGroupModalProps) => {
+    const [ gropName, setGroupName ] = useState("");
+
+    return (
+        <Modal
+            title="New Tag"
+            visible={props.isModalOpen}
+            onOk={() => props.onCreate(gropName)}
+            onCancel={props.closeModal}>
+            <Input placeholder="Group name" value={gropName} onChange={(e) => setGroupName(e.target.value)} />
+        </Modal>
+    )
+}
--- src/components/Groups/GroupsPage.tsx
+++ src/components/Groups/GroupsPage.tsx
@@ -1,9 +1,57 @@
+import { Alert, Skeleton, Space } from "antd";
+import { useTagGroupAddMutation, useTagGroupGetQuery, useTagGroupRemoveMutation } from "api/partial/taggroup";
+import { NewCard } from "components/Common/NewCard/NewCard";
+import { TagContainer } from "components/Common/Tag/TagContainer";
+import { useState } from "react";
+import styled from "styled-components";
+import { CreateTagGroupModal } from "./CreateTagGroupModal";
+
 export const GroupsPage = () => {
+    const [ isCreatingGroup, setIsCreatingGroup ] = useState(false);
+    const { data, isFetching, isError, error } = useTagGroupGetQuery();
+    const [ crateTagGroup, {} ] = useTagGroupAddMutation();
+    const [ removeTagGroup, {} ] = useTagGroupRemoveMutation();
+    
     return (
-        <div>
+        <TagGroupsPageContainer>
             <h1>
-                Groups page
+                Groups
             </h1>
-        </div>
+            
+            {isFetching && <Skeleton.Image active />}
+
+            {isError && <Alert
+                message="Error"
+                description={error.toString()}
+                type="error"
+                showIcon />}
+
+            <ContentContainer>
+                <Space wrap>
+                    {data?.map(tag =>
+                        <TagContainer title={tag.name} onRemove={() => removeTagGroup({id: tag.id})} />)}
+                    <NewCard onClick={() => setIsCreatingGroup(true)}/>
+                </Space>
+            </ContentContainer>
+
+            <CreateTagGroupModal
+                isModalOpen={isCreatingGroup}
+                onCreate={(groupName) => crateTagGroup({
+                    updateGroupCommandModel: {
+                        groupName: groupName,
+                        tagIds: []
+                    }
+                })}
+                closeModal={() => setIsCreatingGroup(false)}/>
+        </TagGroupsPageContainer>
     )
 }
+
+const TagGroupsPageContainer = styled.div`
+    height: 100%;
+`
+
+const ContentContainer = styled.div`
+    overflow: auto;
+    height: 100%;
+`
